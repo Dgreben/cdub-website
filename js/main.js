d28184f995d2ba43dcbcfbbc467259bb72e53012
Dave Greben <dave3greben@gmail.com>
2022-10-15 13:48:26 -0700

    fifth set of updates - final branch (production)

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    $(".cdub-splash-content").fadeIn(3000);
+    $(".player-box").fadeIn(2000);
+    $(".content-column-2").slideDown(2000, "swing");
+    $(".news-pieces").slideDown(2000, "swing");
+    $(".about-content").fadeIn(3000);
+    $(".about-content-2").delay(1000).fadeIn(3000);
+    $(".partners-page p").fadeIn(3000);
+
+
+});
