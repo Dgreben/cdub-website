f439c3c44c5d0de2d45a51019db850ef2a9b5fd2
Dave Greben <dave3greben@gmail.com>
2022-10-17 13:17:58 -0700

    bug fix post-production 2 (mobile)

--- js/main.js
+++ js/main.js
@@ -2,11 +2,10 @@ $(document).ready(function(){
 
     $(".cdub-splash-content").fadeIn(3000);
     $(".player-box").fadeIn(2000);
-    $(".content-column-2").slideDown(2000, "swing");
+    $(".content-column-2").fadeIn(3000);
     $(".news-pieces").slideDown(2000, "swing");
     $(".about-content").fadeIn(3000);
     $(".about-content-2").delay(1000).fadeIn(3000);
     $(".partners-page p").fadeIn(3000);
-
 
 });
